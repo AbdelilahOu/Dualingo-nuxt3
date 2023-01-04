f56386b0b41d0eff2d1046ef9be1a42982129aec
Abdelilah_OU <gxgamer710@gmail.com>
2023-01-04 18:12:04 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="h-screen w-screen">
+    <NuxtPage />
+  </main>
+</template>
+
+<script setup lang="ts">
+import "vue3-lottie/dist/style.css";
+</script>
---
+++ components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="h-16 w-full px-6 border-b-2">
+    <nav
+      class="grid h-16 grid-rows-1 grid-cols-[1fr_82px_82px_82px_82px] items-center"
+    >
+      <div class="h-16 w-fill flex items-center">
+        <div class="fill-dualingo h-7 w-32 pl-4 flex items-center aspect-auto">
+          <UiDualingoLogo />
+        </div>
+      </div>
+      <div class="h-full w-full flex items-center px-[18px]">
+        <img src="../assets/svg/german.svg" alt="" />
+      </div>
+      <div class="h-full w-full flex items-center px-[18px]">
+        <span class="flex items-center">
+          <img src="../assets/svg/streak.svg" alt="" />
+          <span> 80 </span>
+        </span>
+      </div>
+      <div class="h-full w-full flex items-center px-[18px]"></div>
+      <div class="h-full w-full flex items-center px-[18px]"></div>
+    </nav>
+  </header>
+</template>
