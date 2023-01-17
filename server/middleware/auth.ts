3bcbe23618b2c58e342ec7f1e69681d6c87f8cc8
Abdelilah_OU <gxgamer710@gmail.com>
2023-01-17 14:36:04 +0000

    add auth middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,3 +1,52 @@
-export default defineEventHandler((event) => {
+import UrlPattern from "url-pattern";
+import { ValidateToken } from "../utils/jwt";
+
+export default defineEventHandler(async (event) => {
+  const endPoints = [
+    "/api/vote/:id",
+    "/api/vote/create",
+    "/api/vote/created",
+    "/api/question/:id",
+    "/api/question/all",
+    "/api/question/active",
+    "/api/question/create",
+  ];
+  //
+  const IsHandledByThisMiddleware = endPoints.some((endPoint) => {
+    const pattern = new UrlPattern(endPoint);
+    const { url } = event.req;
+    if (url) {
+      return pattern.match(url);
+    }
+    return false;
+  });
+  //
+  if (!IsHandledByThisMiddleware) return;
+  //
+  const token = event.req.headers["authorization"]?.split(" ")[1];
+  //
+  if (!token) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "Unauthorized",
+      })
+    );
+  }
+  //
+  const decoded = ValidateToken(token);
+  //
+  if (!decoded) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "Unauthorized token",
+      })
+    );
+  }
+  console.log("server auth middleware is done");
+  event.context.auth = decoded;
   return;
 });
